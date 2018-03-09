e7f9b1af194450c0a909ec5d0f5eb82ef313c40d
hirura <hirura@gmail.com>
2018-03-09 23:29:30 +0900

    update spec/spec_helper.rb to use simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,11 @@
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+require 'bundler/setup'
+
+if ENV['CI']
+  require 'codeclimate-test-reporter'
+  SimpleCov.start do
+    #add_filter '/spec/'
+  end
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rubyang'
